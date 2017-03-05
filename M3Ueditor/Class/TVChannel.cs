65c53d44579936f165aca85bbdcacbe31a9f1acf
Sergei <hmaster20@gmail.com>
2017-03-05 14:10:20 +0300

    ref

--- M3Ueditor/Class/TVChannel.cs
+++ M3Ueditor/Class/TVChannel.cs
@@ -13,22 +13,7 @@ namespace M3Ueditor
 {
     public class TVChannel
     {
-
-        //private string _tvgName, _Group, _ip, _tvid, _logo;
-        //public event PropertyChangedEventHandler PropertyChanged;
-
-        //public TVChannel(string Name, string ip, string Group = "", string logo = "", string tvid = "", string lol = "")
-        //{
-        //    _tvgName = Name;
-        //    _Group = Group;
-        //    _ip = ip;
-        //    _tvid = tvid;
-        //    _logo = logo;
-        //}
-
-
-        //#EXTINF:-1 tvg-name="Карусель" tvg-logo="@Карусель" group-title="Детям",Карусель
-        public TVChannel(string _tvgName,  string _tvglogo, string _groupTitle, string _udp, string _Name)
+        public TVChannel(string _tvgName, string _tvglogo, string _groupTitle, string _udp, string _Name)
         {
             tvgName = _tvgName;
             tvglogo = _tvglogo;
@@ -42,40 +27,5 @@ namespace M3Ueditor
         public string groupTitle { get; set; }
         public string UDP { get; set; }
         public string Name { get; set; }
-
-        //public string tvgName
-        //{
-        //    get { return _tvgName; }
-        //    set { _tvgName = value; }
-        //}
-
-        //public string tvglogo
-        //{
-        //    get { return _logo; }
-        //    set { _logo = value; }
-        //}
-
-        //public string Group
-        //{
-        //    get { return _Group; }
-        //    set { _Group = value; }
-        //}
-
-        //public string IP
-        //{
-        //    get { return _ip; }
-        //    set { _ip = value; }
-        //}
-
-
-
-
-
-
-        //private void NotifyPropertyChanged(string value)
-        //{
-        //    if (PropertyChanged != null)
-        //        PropertyChanged(this, new PropertyChangedEventArgs(value));
-        //}
     }
 }
