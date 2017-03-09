1e288d35399cf7059985e1761f468d8600afef79
Sergei <hmaster20@gmail.com>
2017-03-09 22:16:56 +0300

    new class

---
+++ M3Ueditor/Class/TVChannelMerge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace M3Ueditor
+{
+    public class TVChannelMerge : TVChannel
+    {
+        public TVChannelMerge(string _tvgName, string _tvglogo, string _groupTitle, string _udp, string _Name, bool _check) : base(_tvgName, _tvglogo, _groupTitle, _udp, _Name)
+        {
+            check = _check;
+        }
+        bool check { get; set; }
+    }
+}
