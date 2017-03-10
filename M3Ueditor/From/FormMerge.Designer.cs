ce18b7249b79399f0c331d949f3695906c29892f
Sergei <hmaster20@gmail.com>
2017-03-10 09:20:40 +0300

    метод проверки дубликатов

--- M3Ueditor/From/FormMerge.Designer.cs
+++ M3Ueditor/From/FormMerge.Designer.cs
@@ -28,56 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.cBoxSelectDubl = new System.Windows.Forms.CheckBox();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button6 = new System.Windows.Forms.Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // checkedListBox
+            // cBoxSelectDubl
             // 
-            this.checkedListBox.FormattingEnabled = true;
-            this.checkedListBox.Location = new System.Drawing.Point(12, 33);
-            this.checkedListBox.Name = "checkedListBox";
-            this.checkedListBox.Size = new System.Drawing.Size(639, 139);
-            this.checkedListBox.TabIndex = 0;
+            this.cBoxSelectDubl.AutoSize = true;
+            this.cBoxSelectDubl.Location = new System.Drawing.Point(184, 8);
+            this.cBoxSelectDubl.Name = "cBoxSelectDubl";
+            this.cBoxSelectDubl.Size = new System.Drawing.Size(142, 17);
+            this.cBoxSelectDubl.TabIndex = 1;
+            this.cBoxSelectDubl.Text = "Выбрать дублирующие";
+            this.cBoxSelectDubl.UseVisualStyleBackColor = true;
+            this.cBoxSelectDubl.CheckedChanged += new System.EventHandler(this.cBoxSelectDubl_CheckedChanged);
             // 
-            // checkBox1
+            // btnApply
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(184, 8);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(142, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "Выбрать дублирующие";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.btnApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnApply.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnApply.Location = new System.Drawing.Point(495, 344);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 2;
+            this.btnApply.Text = "Применить";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
             // 
-            // button3
+            // btnCancel
             // 
-            this.button3.Location = new System.Drawing.Point(294, 344);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Применить";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(576, 344);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "Отмена";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(576, 344);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -108,59 +107,81 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
+            this.Column4,
             this.Column2,
+            this.Column5,
+            this.Column6,
             this.Column3});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 178);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 31);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(639, 160);
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(639, 307);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(12, 353);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 4;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
             // Column1
             // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column1.DataPropertyName = "TvgName";
-            this.Column1.HeaderText = "Column1";
+            this.Column1.HeaderText = "TvgName";
             this.Column1.Name = "Column1";
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.DataPropertyName = "Tvglogo";
+            this.Column4.HeaderText = "Tvglogo";
+            this.Column4.Name = "Column4";
             // 
             // Column2
             // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column2.DataPropertyName = "GroupTitle";
-            this.Column2.HeaderText = "Column2";
+            this.Column2.HeaderText = "GroupTitle";
             this.Column2.Name = "Column2";
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.DataPropertyName = "UDP";
+            this.Column5.HeaderText = "UDP";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.DataPropertyName = "Name";
+            this.Column6.HeaderText = "Name";
+            this.Column6.Name = "Column6";
             // 
             // Column3
             // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
             this.Column3.DataPropertyName = "check";
-            this.Column3.HeaderText = "Column3";
+            this.Column3.HeaderText = "Выбор";
             this.Column3.Name = "Column3";
+            this.Column3.Width = 50;
             // 
             // FormMerge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(663, 379);
-            this.Controls.Add(this.button6);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnApply);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.checkedListBox);
+            this.Controls.Add(this.cBoxSelectDubl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "FormMerge";
             this.Text = "Слияние плейлистов";
@@ -171,18 +192,18 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.CheckedListBox checkedListBox;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox cBoxSelectDubl;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button6;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Column3;
     }
 }
--- M3Ueditor/From/FormMerge.cs
+++ M3Ueditor/From/FormMerge.cs
@@ -26,41 +26,30 @@ namespace M3Ueditor
             NewChannels = new SortableBindingList<TVChannelMerge>();
             ModChannels = new SortableBindingList<TVChannel>();
 
-            // CheckedListBox clb = new CheckedListBox();
-            //  clb.MultiColumn = true;
-
             dataGridView1.AutoGenerateColumns = false;
-
 
             foreach (var item in tvcCurrent)
             {
-                // checkedListBox.Items.Add(item);
-                //clb.Items.Add(item);
-
-                NewChannels.Add(
-                    new TVChannelMerge(
+                NewChannels.Add(new TVChannelMerge(
                                 _tvgName: item.TvgName,
                                 _tvglogo: item.Tvglogo,
                                 _groupTitle: item.GroupTitle,
                                 _udp: item.UDP,
                                 _Name: item.Name,
-                                _check: false)
-                                    );
+                                _check: false));
             }
 
             foreach (var item in tvcMerge)
             {
-                //checkedListBox.Items.Add(item);
-                //clb.Items.Add(item);
-                //NewChannels.Add((TVChannelMerge)item);
+                NewChannels.Add(new TVChannelMerge(
+                _tvgName: item.TvgName,
+                _tvglogo: item.Tvglogo,
+                _groupTitle: item.GroupTitle,
+                _udp: item.UDP,
+                _Name: item.Name,
+                _check: false));
             }
-
-            // checkedListBox.MultiColumn = true;
-            // checkedListBox.Items.AddRange(clb.Items);
-
-
-           dataGridView1.DataSource = NewChannels;
-
+            dataGridView1.DataSource = NewChannels;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -70,7 +59,7 @@ namespace M3Ueditor
 
         private void button6_Click(object sender, EventArgs e)
         {
-          
+
             dataGridView1.ColumnCount = 3;
             dataGridView1.Columns[0].Name = "Product ID";
             dataGridView1.Columns[1].Name = "Product Name";
@@ -90,6 +79,51 @@ namespace M3Ueditor
             chk.HeaderText = "Check Data";
             chk.Name = "chk";
             dataGridView1.Rows[2].Cells[3].Value = true;
+        }
+
+        private void cBoxSelectDubl_CheckedChanged(object sender, EventArgs e)
+        {
+            //if (NewChannels.Count > 0)
+            //{
+            //    NewChannels.Where(x => x.)
+            //}
+
+            for (int ThisRow = 0; ThisRow < dataGridView1.Rows.Count - 1; ThisRow++)
+            {
+               DataGridViewRow CompareRow = dataGridView1.Rows[ThisRow];
+                for (int NextRow = ThisRow + 1; NextRow < dataGridView1.Rows.Count; NextRow++)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[NextRow];
+                    bool DuplicateRow = true;
+
+                    string aa = CompareRow.Cells[1].Value.ToString();
+
+                    //if ((CompareRow.Cells[3]) == (row.Cells[3]))
+                    if ((CompareRow.Cells[3].Value.ToString()) == (row.Cells[3].Value.ToString()))
+                    {
+                        //dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor = Color.Red;
+                        //row.DefaultCellStyle.ForeColor = Color.Silver;
+                        //row.DefaultCellStyle.Font = new Font(TableRec.Font, FontStyle.Strikeout);
+                        // dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Red; 
+
+                        row.DefaultCellStyle.ForeColor = Color.Silver;
+                        row.DefaultCellStyle.Font = new Font(dataGridView1.Font, FontStyle.Strikeout);
+                        row.DefaultCellStyle.BackColor = System.Drawing.Color.Red;
+                        //row.BackColor = System.Drawing.Color.Red;
+                        //CompareRow.BackColor = System.Drawing.Color.Red;
+                        CompareRow.DefaultCellStyle.BackColor = System.Drawing.Color.Red;
+                    }
+                    else if (DuplicateRow)
+                    {
+                        DuplicateRow = false;
+                    }
+                }
+            }
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
