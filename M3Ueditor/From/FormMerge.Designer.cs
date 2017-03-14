085e7534f993b54b8027929889c7610494364584
Sergei <hmaster20@gmail.com>
2017-03-14 09:06:05 +0300

    add new icon for new button

--- M3Ueditor/From/FormMerge.Designer.cs
+++ M3Ueditor/From/FormMerge.Designer.cs
@@ -31,9 +31,7 @@
             this.cBoxSelectDubl = new System.Windows.Forms.CheckBox();
             this.btnApply = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.bntDelete = new System.Windows.Forms.Button();
             this.dgvMerge = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,17 +40,19 @@
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.infolabel = new System.Windows.Forms.Label();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.btnUnSelectAll = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMerge)).BeginInit();
             this.SuspendLayout();
             // 
             // cBoxSelectDubl
             // 
             this.cBoxSelectDubl.AutoSize = true;
-            this.cBoxSelectDubl.Location = new System.Drawing.Point(184, 8);
+            this.cBoxSelectDubl.Location = new System.Drawing.Point(76, 6);
             this.cBoxSelectDubl.Name = "cBoxSelectDubl";
-            this.cBoxSelectDubl.Size = new System.Drawing.Size(142, 17);
+            this.cBoxSelectDubl.Size = new System.Drawing.Size(148, 17);
             this.cBoxSelectDubl.TabIndex = 1;
-            this.cBoxSelectDubl.Text = "Выбрать дублирующие";
+            this.cBoxSelectDubl.Text = "Выбрать все дубликаты";
             this.cBoxSelectDubl.UseVisualStyleBackColor = true;
             this.cBoxSelectDubl.CheckedChanged += new System.EventHandler(this.cBoxSelectDubl_CheckedChanged);
             // 
@@ -66,6 +66,7 @@
             this.btnApply.TabIndex = 2;
             this.btnApply.Text = "Применить";
             this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
             // 
             // btnCancel
             // 
@@ -78,32 +79,16 @@
             this.btnCancel.Text = "Отмена";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // bntDelete
             // 
-            this.button2.Location = new System.Drawing.Point(12, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(80, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Выбрать все";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(98, 4);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(80, 23);
-            this.button5.TabIndex = 2;
-            this.button5.Text = "Снять выбор";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(506, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Удалить все выбраное";
-            this.button1.UseVisualStyleBackColor = true;
+            this.bntDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bntDelete.Location = new System.Drawing.Point(475, 4);
+            this.bntDelete.Name = "bntDelete";
+            this.bntDelete.Size = new System.Drawing.Size(176, 23);
+            this.bntDelete.TabIndex = 2;
+            this.bntDelete.Text = "Удалить выбранные дубликаты";
+            this.bntDelete.UseVisualStyleBackColor = true;
+            this.bntDelete.Click += new System.EventHandler(this.bntDelete_Click);
             // 
             // dgvMerge
             // 
@@ -171,7 +156,7 @@
             // 
             this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
             this.Column3.DataPropertyName = "check";
-            this.Column3.HeaderText = "Выбор";
+            this.Column3.HeaderText = "Дубль";
             this.Column3.Name = "Column3";
             this.Column3.Width = 50;
             // 
@@ -183,18 +168,39 @@
             this.infolabel.Size = new System.Drawing.Size(212, 23);
             this.infolabel.TabIndex = 4;
             // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Image = global::M3Ueditor.Properties.Resources.SelectAll;
+            this.btnSelectAll.Location = new System.Drawing.Point(12, 2);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(23, 23);
+            this.btnSelectAll.TabIndex = 5;
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // btnUnSelectAll
+            // 
+            this.btnUnSelectAll.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnUnSelectAll.Image = global::M3Ueditor.Properties.Resources.unSelectAll;
+            this.btnUnSelectAll.Location = new System.Drawing.Point(41, 2);
+            this.btnUnSelectAll.Name = "btnUnSelectAll";
+            this.btnUnSelectAll.Size = new System.Drawing.Size(23, 23);
+            this.btnUnSelectAll.TabIndex = 5;
+            this.btnUnSelectAll.UseVisualStyleBackColor = true;
+            this.btnUnSelectAll.Click += new System.EventHandler(this.btnUnSelectAll_Click);
+            // 
             // FormMerge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(663, 379);
+            this.Controls.Add(this.btnUnSelectAll);
+            this.Controls.Add(this.btnSelectAll);
             this.Controls.Add(this.infolabel);
             this.Controls.Add(this.dgvMerge);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bntDelete);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnApply);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.cBoxSelectDubl);
             this.Name = "FormMerge";
             this.Text = "Слияние плейлистов";
@@ -208,9 +214,7 @@
         private System.Windows.Forms.CheckBox cBoxSelectDubl;
         private System.Windows.Forms.Button btnApply;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bntDelete;
         private System.Windows.Forms.DataGridView dgvMerge;
         private System.Windows.Forms.Label infolabel;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
@@ -219,5 +223,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Column3;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.Button btnUnSelectAll;
     }
 }
--- M3Ueditor/Properties/Resources.Designer.cs
+++ M3Ueditor/Properties/Resources.Designer.cs
@@ -149,5 +149,25 @@ namespace M3Ueditor.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SelectAll {
+            get {
+                object obj = ResourceManager.GetObject("SelectAll", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap unSelectAll {
+            get {
+                object obj = ResourceManager.GetObject("unSelectAll", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
