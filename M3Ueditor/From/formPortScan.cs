482ece0c95f691e662cd348cb1782c15cb6a6abd
Sergei <hmaster20@gmail.com>
2017-03-30 22:14:34 +0300

    scan

--- M3Ueditor/From/formPortScan.cs
+++ M3Ueditor/From/formPortScan.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +15,26 @@ namespace M3Ueditor
         public formPortScan()
         {
             InitializeComponent();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            IPAddress ipstart = IPAddress.Parse(ipStart.Text);
+            // IPAddress ipstart = new IPAddress(ipStart.Text);
+            // IPAddress ipstop = new IPAddress(ipAddressControl2.GetAddressBytes());
+            IPAddress ipstop = IPAddress.Parse(ipEnd.Text);
+
+            if (ipstart.ToString() == "0.0.0.0" || ipstop.ToString() == "0.0.0.0")
+                return;
+
+            // parent.Invoke(new delegateParentThread(parent.vlcStop));
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            Scanner scanner = new Scanner(btnStart, btnStop, progressBar1, label9, label18, dataGridView1);
+            int timeout = (int)numericUpDown2.Value;
+            int port = (int)numericUpDown3.Value;
+            scanner.StartScann(ipstart, ipstop, port, timeout);
         }
     }
 }
