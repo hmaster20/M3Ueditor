376bf9bd76da6907e0906f42da129d523c601eae
Sergei <hmaster20@gmail.com>
2017-04-10 09:55:40 +0300

    рефакторинг

--- M3Ueditor/Class/Ethernet.cs
+++ M3Ueditor/Class/Ethernet.cs
@@ -1,12 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace M3Ueditor
 {
     class Ethernet
     {
+        /// <summary>Получает первый IP-адрес на активной сетевой карте</summary>
+        /// <returns>Возвращает IP-адрес в виде строки (string)</returns>
+        public static string getLocalIP()
+        {
+            string ipstring = "";
+            if (NetworkInterface.GetIsNetworkA‌​vailable())
+            {
+                NetworkInterface[] alladapter =
+                    NetworkInterface.GetAllNetworkInterfaces().Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Ethernet).ToArray();
 
+                NetworkInterface ActiveAdapter = alladapter.First(x => x.OperationalStatus == OperationalStatus.Up);
+                PhysicalAddress address = ActiveAdapter.GetPhysicalAddress();
+                UnicastIPAddressInformationCollection ip = ActiveAdapter.GetIPProperties().UnicastAddresses;
+                ipstring = ip[0].Address.ToString();
+            }
+            return ipstring;
+        }
+
+
+        /// <summary>Возвращает количество адресов для сканирования</summary>
+        /// <param name="start">Начальный IP-адрес</param>
+        /// <param name="stop">Конечный IP-адрес</param>
+        /// <returns>Число в формате uint</returns>
+        public static uint calculateNumOfIpAddr(IPAddress start, IPAddress stop)
+        {
+            byte[] str = start.GetAddressBytes();
+            byte[] stp = stop.GetAddressBytes();
+
+            uint sta = (uint)255 * 255 * 255 * str[0] + (uint)255 * 255 * str[1] + (uint)255 * str[2] + (uint)str[3];
+            uint stb = (uint)255 * 255 * 255 * stp[0] + (uint)255 * 255 * stp[1] + (uint)255 * stp[2] + (uint)stp[3];
+
+            if (sta > stb)
+                return (sta - stb) + 1;
+            else
+                return (stb - sta) + 1;
+        }
+
+
+        /// <summary>Направление поиска (прямое или обратное)</summary>
+        /// <param name="start">Начальный IP-адрес</param>
+        /// <param name="stop">Конечный IP-адрес</param>
+        /// <returns>True или False</returns>
+        public static bool searchForward(IPAddress start, IPAddress stop)
+        {
+            byte[] str = start.GetAddressBytes();
+            byte[] stp = stop.GetAddressBytes();
+
+            uint sta = (uint)255 * 255 * 255 * str[0] + (uint)255 * 255 * str[1] + (uint)255 * str[2] + (uint)str[3];
+            uint stb = (uint)255 * 255 * 255 * stp[0] + (uint)255 * 255 * stp[1] + (uint)255 * stp[2] + (uint)stp[3];
+
+            if (sta > stb)
+                return false;
+            else
+                return true;
+        }
     }
 }
--- M3Ueditor/Class/Scanner.cs
+++ M3Ueditor/Class/Scanner.cs
@@ -52,24 +52,9 @@ namespace M3Ueditor
             CurrentIP_label = CurrentIP;
             FoundIP_label = FoundIP;
             FindCH = FindChannels;
-            localhost = getLocalIP();
+            localhost = Ethernet.getLocalIP();
         }
 
-        private string getLocalIP()
-        {
-            string ipstring = "";
-            if (NetworkInterface.GetIsNetworkA‌​vailable())
-            {
-                NetworkInterface[] alladapter =
-                    NetworkInterface.GetAllNetworkInterfaces().Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Ethernet).ToArray();
-
-                NetworkInterface ActiveAdapter = alladapter.First(x => x.OperationalStatus == OperationalStatus.Up);
-                PhysicalAddress address = ActiveAdapter.GetPhysicalAddress();
-                UnicastIPAddressInformationCollection ip = ActiveAdapter.GetIPProperties().UnicastAddresses;
-                ipstring = ip[0].Address.ToString();
-            }
-            return ipstring;
-        }
 
         /// <summary>Start scanning</summary>
         public void StartScann(IPAddress startIP, IPAddress endIP, int prt, int tmo)
@@ -95,8 +80,8 @@ namespace M3Ueditor
             int recv = 0;
             bool foundchannel = false;
             bool timedout = false;
-            bool searchforward = searchForward(IPstart, IPstop);
-            uint progressfull = calculateNumOfIpAddr(IPstart, IPstop);
+            bool searchforward = Ethernet.searchForward(IPstart, IPstop);
+            uint progressfull = Ethernet.calculateNumOfIpAddr(IPstart, IPstop);
             bool stopcondition = false;
             byte[] oct = IPstart.GetAddressBytes();
             multiep = new IPEndPoint(new IPAddress(oct), Port);
@@ -261,46 +246,7 @@ namespace M3Ueditor
             //Abort thread
             ThreadReceiver.Abort();
         }
-
-
-        /// <summary>Возвращает количество адресов для сканирования</summary>
-        /// <param name="start">Начальный IP-адрес</param>
-        /// <param name="stop">Конечный IP-адрес</param>
-        /// <returns>Число в формате uint</returns>
-        private uint calculateNumOfIpAddr(IPAddress start, IPAddress stop)
-        {
-            byte[] str = start.GetAddressBytes();
-            byte[] stp = stop.GetAddressBytes();
-
-            uint sta = (uint)255 * 255 * 255 * str[0] + (uint)255 * 255 * str[1] + (uint)255 * str[2] + (uint)str[3];
-            uint stb = (uint)255 * 255 * 255 * stp[0] + (uint)255 * 255 * stp[1] + (uint)255 * stp[2] + (uint)stp[3];
-
-            if (sta > stb)
-                return (sta - stb) + 1;
-            else
-                return (stb - sta) + 1;
-        }
-
-
-        /// <summary>Направление поиска (прямое или обратное)</summary>
-        /// <param name="start">Начальный IP-адрес</param>
-        /// <param name="stop">Конечный IP-адрес</param>
-        /// <returns>True или False</returns>
-        private bool searchForward(IPAddress start, IPAddress stop)
-        {
-            byte[] str = start.GetAddressBytes();
-            byte[] stp = stop.GetAddressBytes();
-
-            uint sta = (uint)255 * 255 * 255 * str[0] + (uint)255 * 255 * str[1] + (uint)255 * str[2] + (uint)str[3];
-            uint stb = (uint)255 * 255 * 255 * stp[0] + (uint)255 * 255 * stp[1] + (uint)255 * stp[2] + (uint)stp[3];
-
-            if (sta > stb)
-                return false;
-            else
-                return true;
-        }
-
-
+        
         public void TableAdd(TVChannel tvc)
         {
             FindCH.Add(tvc);
