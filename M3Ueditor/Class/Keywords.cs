291bf6ee04585074b6c8fbc435830dc5cbc5c046
hmaster20 <hmaster20@gmail.com>
2018-01-17 23:54:16 +0300

    файл для разборки по ключам

---
+++ M3Ueditor/Class/Keywords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace M3Ueditor
+{
+    public class Keywords
+    {
+        string[] keys = { "url-tvg", "cache" , "deinterlace ", "aspect-ratio", "croppadd",  };
+       
+
+   
+    }
+}
+
+//(#EXTM3U)(.*)(#EXTINF)(.*)(#EXTINF)
+//(#EXTINF).*\s\S.*
+//(#EXTINF)(.*\s\S.*)
+//http://cc.davelozinski.com/c-sharp/the-fastest-way-to-read-and-process-text-files
