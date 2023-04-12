21a8036ae0a5de834e3ba44c104d342371c878e0
hmaster20 <hmaster20@gmail.com>
2023-04-12 11:12:37 +0300

    redesign

--- M3Ueditor/From/formMain.Designer.cs
+++ M3Ueditor/From/formMain.Designer.cs
@@ -516,12 +516,11 @@
             // 
             // btnSaveGlobal
             // 
-            this.btnSaveGlobal.Image = global::M3Ueditor.Properties.Resources.save;
             resources.ApplyResources(this.btnSaveGlobal, "btnSaveGlobal");
+            this.btnSaveGlobal.Image = global::M3Ueditor.Properties.Resources.save;
             this.btnSaveGlobal.Name = "btnSaveGlobal";
             this.btnSaveGlobal.UseVisualStyleBackColor = true;
             // 
-
             // textBoxGlobal
             // 
             resources.ApplyResources(this.textBoxGlobal, "textBoxGlobal");
