480b912999f0ecfe076d42edc9e92ff31ca46f5a
hmaster20 <hmaster20@gmail.com>
2023-04-15 12:05:34 +0300

    доравботка метода парсинга refactoring

--- M3Ueditor/Class/Helper.cs
+++ M3Ueditor/Class/Helper.cs
@@ -89,8 +89,8 @@ namespace M3Ueditor
         }
 
 
-        /// <summary>Парсит файл и создает список строк каналов</summary>
-        public static List<string> getRawChannels(string playlist)
+        /// <summary>Парсит содержимое файла и создает список каналов</summary>
+        private static List<string> getRawChannels(string playlist)
         {
             string data = playlist;
             var start1 = data.IndexOf(@"#EXTM3U") + 7;
@@ -154,14 +154,20 @@ namespace M3Ueditor
 
         public static SortableBindingList<TVChannel> getChannels(string fullName)
         {
-            List<string> RawChannels = getRawChannels(fullName);
-
             SortableBindingList<TVChannel> channels = new SortableBindingList<TVChannel>();
 
-            for (int i = 0; i < RawChannels.Count; i++)
+            using (StreamReader playlist = new StreamReader(fullName))
             {
+                string fullTextFile = playlist.ReadToEnd();
 
+                List<string> RawChannels = getRawChannels(fullTextFile);
+
+                for (int i = 0; i < RawChannels.Count; i++)
+                {
+
+                }
             }
+
             return channels;
         }
 
@@ -241,6 +247,79 @@ namespace M3Ueditor
         //        // GetListChannel = // List<TVChannel>
         //    }
         //}
+
+
+
+
+        // БЫЛ БЕЗ ССЫЛОК, возможно на удаление
+        public void ParseM3UtestV2(string fullName)
+        {
+            using (StreamReader playlist = new StreamReader(fullName))
+            {
+                //string fullTextFile = playlist.ReadToEnd();
+                //textBoxGlobal.Text = Helper.getGlobalParams(fullTextFile);
+
+                //Helper.Options(fullTextFile);
+                // channels = Helper.Options(fullTextFile);
+
+                // GetGlobalOption = // string
+                // GetListChannel = // List<TVChannel>
+
+
+                List<TVChannel> channel = new List<TVChannel>();
+
+                //string readText = File.ReadAllText(path);
+                //sr.ReadToEnd()           
+
+
+
+                //string test = playlist.ReadToEnd();
+
+                //Regex rm3 = new Regex(@"#EXTM3U.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                //MatchCollection matchesM = rm3.Matches(test);
+                //Regex rin = new Regex(@"#EXTINF.*\s\S.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                //MatchCollection matchesI = rin.Matches(test);
+
+
+                string line = "";
+                while ((line = playlist.ReadLine()) != null)
+                {
+                    string pattern = @"#EXTM3U.*";
+                    Match m = Regex.Match(line, pattern);
+                    Match ml = Regex.Match(line, @"#EXTINF.*\s\S.*");
+
+                    //Regex rx = new Regex(@"\b(?<word>\w+)\s+(\k<word>)\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    Regex rx = new Regex(@"#.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    // Define a test string.        
+                    //string text = "The the quick brown fox  fox jumped over the lazy dog dog.";
+
+                    // Find matches.
+                    MatchCollection matches = rx.Matches(line);
+
+
+                    if (line.StartsWith("#EXTM3U"))
+                    {
+                        // continue;
+                        //
+
+                        string pattern2 = @"#EXTM3U.*";
+                        Match m2 = Regex.Match(line, pattern2);
+
+                        //string text = "ImageDimension=655x0;ThumbnailDimension=0x0";
+                        //Regex pattern = new Regex(@"#EXTM3U.*#EXTINF");
+                        //Match match = pattern.Match(line);
+                        //int imageWidth = int.Parse(match.Groups["imageWidth"].Value);
+                        //int imageHeight = int.Parse(match.Groups["imageHeight"].Value);
+                        //int thumbWidth = int.Parse(match.Groups["thumbWidth"].Value);
+                        //int thumbHeight = int.Parse(match.Groups["thumbHeight"].Value);
+                    }
+                }
+            }
+        }
+
+
+
+
 
 
     }
