d17b8743b7ab13f1419f1c7f313d8f8311d56dc3
hmaster20 <hmaster20@gmail.com>
2023-04-16 16:33:55 +0300

    Comment unused code

--- M3Ueditor/Class/Helper.cs
+++ M3Ueditor/Class/Helper.cs
@@ -238,68 +238,68 @@ namespace M3Ueditor
             return channels;
         }
 
-        public static SortableBindingList<TVChannel> ParseM3U(string fullName)
-        {
-            StreamReader playlist = new StreamReader(fullName);
+        //public static SortableBindingList<TVChannel> ParseM3U(string fullName)
+        //{
+        //    StreamReader playlist = new StreamReader(fullName);
 
-            SortableBindingList<TVChannel> ListTV = new SortableBindingList<TVChannel>();
+        //    SortableBindingList<TVChannel> ListTV = new SortableBindingList<TVChannel>();
 
-            string line = "";
+        //    string line = "";
 
-            string tvgName = "N/A";
-            string tvglogo = "N/A";
-            string groupTitle = "N/A";
-            string Name = "N/A";
-            string udp = "N/A";
+        //    string tvgName = "N/A";
+        //    string tvglogo = "N/A";
+        //    string groupTitle = "N/A";
+        //    string Name = "N/A";
+        //    string udp = "N/A";
 
-            while ((line = playlist.ReadLine()) != null)
-            {
-                if (line.StartsWith("#EXTM3U"))
-                {
-                    continue;
-                }
-                if (line.StartsWith("#EXTINF"))
-                {
-                    tvgName = stringOperations.Between(line, "tvg-name=\"", "\"");
-                    tvglogo = stringOperations.Between(line, "tvg-logo=\"", "\"");
-                    groupTitle = stringOperations.Between(line, "group-title=\"", "\"");
-                    Name = line.Split(',').Last();
-                    continue;
-                }
-                else if (line.Contains("//"))
-                {
-                    udp = line;
-                }
-                else
-                {
-                    continue;
-                }
+        //    while ((line = playlist.ReadLine()) != null)
+        //    {
+        //        if (line.StartsWith("#EXTM3U"))
+        //        {
+        //            continue;
+        //        }
+        //        if (line.StartsWith("#EXTINF"))
+        //        {
+        //            tvgName = stringOperations.Between(line, "tvg-name=\"", "\"");
+        //            tvglogo = stringOperations.Between(line, "tvg-logo=\"", "\"");
+        //            groupTitle = stringOperations.Between(line, "group-title=\"", "\"");
+        //            Name = line.Split(',').Last();
+        //            continue;
+        //        }
+        //        else if (line.Contains("//"))
+        //        {
+        //            udp = line;
+        //        }
+        //        else
+        //        {
+        //            continue;
+        //        }
 
-                try
-                {
-                    ListTV.Add(new TVChannel(
-                        _tvgName: tvgName.Trim(),
-                        _tvglogo: tvglogo.Trim(),
-                        _groupTitle: groupTitle.Trim(),
-                        _udp: udp.Trim(),
-                        _Name: Name.Trim()
-                        ));
-                }
-                catch (ArgumentOutOfRangeException)
-                {
-                    MessageBox.Show("A channel has been omitted due to its incorrect format");
-                    continue;
-                }
-            }
-            playlist.Close();
+        //        try
+        //        {
+        //            ListTV.Add(new TVChannel(
+        //                _tvgName: tvgName.Trim(),
+        //                _tvglogo: tvglogo.Trim(),
+        //                _groupTitle: groupTitle.Trim(),
+        //                _udp: udp.Trim(),
+        //                _Name: Name.Trim()
+        //                ));
+        //        }
+        //        catch (ArgumentOutOfRangeException)
+        //        {
+        //            MessageBox.Show("A channel has been omitted due to its incorrect format");
+        //            continue;
+        //        }
+        //    }
+        //    playlist.Close();
 
-            if (ListTV.Count == 0)
-            {
-                MessageBox.Show("Структура файла не распознана!", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                //fileName = null;
-            }
-            return ListTV;
-        }
+        //    if (ListTV.Count == 0)
+        //    {
+        //        MessageBox.Show("Структура файла не распознана!", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //        //fileName = null;
+        //    }
+        //    return ListTV;
+        //}
 
 
         //public void ParseM3Utest(string fullName)
@@ -316,73 +316,68 @@ namespace M3Ueditor
         //}
 
 
+        //public void ParseM3UtestV2(string fullName)
+        //{
+        //    using (StreamReader playlist = new StreamReader(fullName))
+        //    {
+        //        //string fullTextFile = playlist.ReadToEnd();
+        //        //textBoxGlobal.Text = Helper.getGlobalParams(fullTextFile);
+
+        //        //Helper.Options(fullTextFile);
+        //        // channels = Helper.Options(fullTextFile);
+
+        //        // GetGlobalOption = // string
+        //        // GetListChannel = // List<TVChannel>
 
 
-        // БЫЛ БЕЗ ССЫЛОК, возможно на удаление
-        public void ParseM3UtestV2(string fullName)
-        {
-            using (StreamReader playlist = new StreamReader(fullName))
-            {
-                //string fullTextFile = playlist.ReadToEnd();
-                //textBoxGlobal.Text = Helper.getGlobalParams(fullTextFile);
+        //        List<TVChannel> channel = new List<TVChannel>();
 
-                //Helper.Options(fullTextFile);
-                // channels = Helper.Options(fullTextFile);
+        //        //string readText = File.ReadAllText(path);
+        //        //sr.ReadToEnd()           
+        
+        //        //string test = playlist.ReadToEnd();
 
-                // GetGlobalOption = // string
-                // GetListChannel = // List<TVChannel>
+        //        //Regex rm3 = new Regex(@"#EXTM3U.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        //        //MatchCollection matchesM = rm3.Matches(test);
+        //        //Regex rin = new Regex(@"#EXTINF.*\s\S.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        //        //MatchCollection matchesI = rin.Matches(test);
 
 
-                List<TVChannel> channel = new List<TVChannel>();
+        //        string line = "";
+        //        while ((line = playlist.ReadLine()) != null)
+        //        {
+        //            string pattern = @"#EXTM3U.*";
+        //            Match m = Regex.Match(line, pattern);
+        //            Match ml = Regex.Match(line, @"#EXTINF.*\s\S.*");
 
-                //string readText = File.ReadAllText(path);
-                //sr.ReadToEnd()           
+        //            //Regex rx = new Regex(@"\b(?<word>\w+)\s+(\k<word>)\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        //            Regex rx = new Regex(@"#.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        //            // Define a test string.        
+        //            //string text = "The the quick brown fox  fox jumped over the lazy dog dog.";
 
-
-
-                //string test = playlist.ReadToEnd();
-
-                //Regex rm3 = new Regex(@"#EXTM3U.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                //MatchCollection matchesM = rm3.Matches(test);
-                //Regex rin = new Regex(@"#EXTINF.*\s\S.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                //MatchCollection matchesI = rin.Matches(test);
-
-
-                string line = "";
-                while ((line = playlist.ReadLine()) != null)
-                {
-                    string pattern = @"#EXTM3U.*";
-                    Match m = Regex.Match(line, pattern);
-                    Match ml = Regex.Match(line, @"#EXTINF.*\s\S.*");
-
-                    //Regex rx = new Regex(@"\b(?<word>\w+)\s+(\k<word>)\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                    Regex rx = new Regex(@"#.*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                    // Define a test string.        
-                    //string text = "The the quick brown fox  fox jumped over the lazy dog dog.";
-
-                    // Find matches.
-                    MatchCollection matches = rx.Matches(line);
+        //            // Find matches.
+        //            MatchCollection matches = rx.Matches(line);
 
 
-                    if (line.StartsWith("#EXTM3U"))
-                    {
-                        // continue;
-                        //
+        //            if (line.StartsWith("#EXTM3U"))
+        //            {
+        //                // continue;
+        //                //
 
-                        string pattern2 = @"#EXTM3U.*";
-                        Match m2 = Regex.Match(line, pattern2);
+        //                string pattern2 = @"#EXTM3U.*";
+        //                Match m2 = Regex.Match(line, pattern2);
 
-                        //string text = "ImageDimension=655x0;ThumbnailDimension=0x0";
-                        //Regex pattern = new Regex(@"#EXTM3U.*#EXTINF");
-                        //Match match = pattern.Match(line);
-                        //int imageWidth = int.Parse(match.Groups["imageWidth"].Value);
-                        //int imageHeight = int.Parse(match.Groups["imageHeight"].Value);
-                        //int thumbWidth = int.Parse(match.Groups["thumbWidth"].Value);
-                        //int thumbHeight = int.Parse(match.Groups["thumbHeight"].Value);
-                    }
-                }
-            }
-        }
+        //                //string text = "ImageDimension=655x0;ThumbnailDimension=0x0";
+        //                //Regex pattern = new Regex(@"#EXTM3U.*#EXTINF");
+        //                //Match match = pattern.Match(line);
+        //                //int imageWidth = int.Parse(match.Groups["imageWidth"].Value);
+        //                //int imageHeight = int.Parse(match.Groups["imageHeight"].Value);
+        //                //int thumbWidth = int.Parse(match.Groups["thumbWidth"].Value);
+        //                //int thumbHeight = int.Parse(match.Groups["thumbHeight"].Value);
+        //            }
+        //        }
+        //    }
+        //}
 
 
 
