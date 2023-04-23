630d246ae879fbe66a007d85e82942389dca843b
hmaster20 <hmaster20@gmail.com>
2023-04-23 13:14:46 +0300

    refactoring

--- M3Ueditor/From/formMain.cs
+++ M3Ueditor/From/formMain.cs
@@ -279,14 +279,14 @@ namespace M3Ueditor
                 string tvglogo = "New Logo";
                 string groupTitle = "New Group";
                 string Name = "New Channel";
-                string udp = "udp://@224.1.1.1:6000";
+                string protocol = "udp://@224.1.1.1:6000";
                 string addon = "";
 
                 channels.Add(new TVChannel(
                             _tvgName: tvgName.Trim(),
                             _tvglogo: tvglogo.Trim(),
                             _groupTitle: groupTitle.Trim(),
-                            _address: udp.Trim(),
+                            _address: protocol.Trim(),
                             _Name: Name.Trim(),
                             _Addon: addon
                             ));
