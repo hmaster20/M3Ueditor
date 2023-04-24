2594411bf8aca766f7652ad849d8dea334d863cc
hmaster20 <hmaster20@gmail.com>
2023-04-24 19:39:08 +0300

    refactoring

--- M3Ueditor/Class/Helper.cs
+++ M3Ueditor/Class/Helper.cs
@@ -38,25 +38,21 @@ namespace M3Ueditor
             {
                 using (StreamReader RAWlist = new StreamReader(fullName))
                 {
-                    string playlist = RAWlist.ReadToEnd();
+                    string raw = RAWlist.ReadToEnd();
 
                     // V1 - 100% рабочий вариант (но трудно читаемый)
-                    //string global = playlist.Split(new string[] { @tagHeader },
+                    //string global = raw.Split(new string[] { @tagHeader },
                     //    StringSplitOptions.None)[1]
                     //    .Split(@tagChannel.ToCharArray())[0]
                     //    .Trim();
 
                     // V2 - По идее код более понятный, хотя его больше
-                    string raw = playlist;
                     var F1 = raw.IndexOf(@tagHeader) + tagHeader.Length;
                     var F2 = raw.IndexOf(@tagChannel);
-                    string RESULT = "";
-                    RESULT = raw.Substring(F1, F2 - F1);
-                    RESULT = Regex.Replace(RESULT, @"\t|\n|\r", "");
-                    RESULT = RESULT.Trim();
-
-                    string global = RESULT;
-
+                    string global = "";
+                    global = raw.Substring(F1, F2 - F1);
+                    global = Regex.Replace(global, @"\t|\n|\r", "");
+                    global = global.Trim();
                     if (global.Length > 0)
                     {
                         global = tagHeader + " " + global;
