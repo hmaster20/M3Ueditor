dfe6bba183161ed38071c1fdb46274a02618b6ff
Serg <hmaster20@gmail.com>
2023-04-29 09:10:26 +0300

    add params to run

--- M3Ueditor/Program.cs
+++ M3Ueditor/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using M3Ueditor.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +10,43 @@ namespace M3Ueditor
 {
     static class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+        private const int ATTACH_PARENT_PROCESS = -1;
+
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            //  if (args.Count() > 0 && args[0] == "version")
+            if (args.Count() > 0 && (args[0] == "--version" || args[0] == "--ver"))
+            {
+                // Перенаправить вывод консоли в родительский процесс (перед любыми вызовами Console.WriteLine())
+                AttachConsole(ATTACH_PARENT_PROCESS);
+
+                Console.WriteLine(About.AssemblyVersion);
+
+                //if (args[0] == "--version" || args[0] == "--ver")
+                //{
+                //    Console.WriteLine(About.AssemblyVersion);
+                //}
+ 
+                //for (int i = 0; i < args.Length; i++)
+                //{
+                //    Console.WriteLine(args[i]);
+                //}
+
+                // Чтобы не ждать нажатия Enter в консоли
+                SendKeys.SendWait("{ENTER}");
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Main());
+            }
         }
     }
 }
